f0f68ff9cadddab82c1fb02efb544d07561adb81
Lucas Procopio <lucas@eadskill.com.br>
2019-08-16 11:39:34 -0300

    feat: Create users API

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -14,6 +15,13 @@ class User extends Model {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
     return this;
   }
 }
