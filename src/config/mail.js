e429289b5d745748e30ee7fce2372780855a243c
Lucas Procopio <procmode.dev@gmailcom>
2019-08-31 13:03:01 -0300

    feat: Add email notification for meetup subscriptions

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4878c55cb814f0',
+    pass: '048189c919d570',
+  },
+  default: {
+    from: 'MeetApp Team <noreply@meetapp.com>',
+  },
+};
